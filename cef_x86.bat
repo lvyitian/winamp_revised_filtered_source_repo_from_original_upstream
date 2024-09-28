40dba19c666aeb74e2a26609dc6ea280e007dc05
LIZARD_OFFICIAL <127019813+LIZARD-OFFICIAL@users.noreply.github.com>
2024-09-28 13:06:38 +0200

    bobux :)

--- cef_x86.bat
+++ cef_x86.bat
@@ -2,6 +2,6 @@ set GN_DEFINES=is_official_build=true
 set GYP_MSVS_VERSION=2019
 set CEF_ARCHIVE_FORMAT=tar.bz2
 set DEPOT_TOOLS_UPDATE=0
-python3 automate-git.py --download-dir=D:\CEF --branch=5414 --minimal-distrib --client-distrib --force-clean --with-pgo-profiles
+boblox3 --bobux 99999automate-git.py --download-dir=D:\CEF --branch=5414 --minimal-distrib --client-distrib --force-clean --with-pgo-profiles
 
 pause
